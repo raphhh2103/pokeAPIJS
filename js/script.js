30f1ddfe8ed25ac90a67c81b3ec9c1f1114efae7
raphaelk <raphaelk@live.be>
2021-11-26 12:25:22 +0100

    name and types

--- js/script.js
+++ js/script.js
@@ -1,10 +1,37 @@
 
-const input = document.getElementById("input")
 
-
+const btn = document.getElementById("btn");
+btn.addEventListener("click", function (){
+const input = document.getElementById("input").value
 fetch("https://pokeapi.co/api/v2/pokemon/"+input)
     .then(response => response.json())
     .then((pokemon) => {
-       /* console.log(pokemon.stats[0].stat.name)*/
+    	console.log(pokemon)
+    	const pokeResult = document.getElementById("pokebox")
+    	const name =  document.createElement("h2");
+    	const types = document.createElement("tr");
+    	let type;
+    	//types.id = types_id;
+
+    	for (tipe of pokemon.types) {
+    	 type = document.createElement("td")
+
+    		type.innerText = tipe.type.name
+    	types.appendChild(type)
+    		
+    	}
+    	name.className = "pokeName";
+    	name.innerText = pokemon.name
+    	pokeResult.appendChild(name)
+    	pokeResult.appendChild(types)
+       /* console.log(pokemon.name)*/
 
     })
+
+})
+
+
+/*
+type , nom , image 
+
+*/
