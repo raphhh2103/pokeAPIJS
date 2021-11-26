c808bde4158740504290eafc9bb0941ba8e57dd5
raphaelk <raphaelk@live.be>
2021-11-26 16:14:35 +0100

    generation of a table for bootstrap

--- js/script.js
+++ js/script.js
@@ -3,23 +3,28 @@
 const btn = document.getElementById("btn");
 btn.addEventListener("click", function (){
 const input = document.getElementById("input").value
+let type;
+const pokeResult = document.getElementById("pokebox")
 fetch("https://pokeapi.co/api/v2/pokemon/"+input)
     .then(response => response.json())
     .then((pokemon) => {
+
+clearfunction(pokeResult)
+//pokeResult.innerHTML = " ";
+
     	console.log(pokemon)
-    	const pokeResult = document.getElementById("pokebox")
+    	
     	const name =  document.createElement("h2");
+    	const table = document.createElement("table")
     	const types = document.createElement("tr");
-    	let type;
+    	const sprite = document.createElement("img")
+    	
     	//types.id = types_id;
+listFunction(types, pokemon)
+imgFunction(sprite,pokemon,pokeResult)
+table.className = " table table-hover"
 
-    	for (tipe of pokemon.types) {
-    	 type = document.createElement("td")
-
-    		type.innerText = tipe.type.name
-    	types.appendChild(type)
-    		
-    	}
+    	
     	name.className = "pokeName";
     	name.innerText = pokemon.name
     	pokeResult.appendChild(name)
@@ -29,9 +34,28 @@ fetch("https://pokeapi.co/api/v2/pokemon/"+input)
     })
 
 })
+const listFunction = function(elementHtml, list){
+for (tipe of list.types) {
+    	 type = document.createElement("td")
+    	 elementHtml.className = " table-secondary"
+			type.className= " row"
+    		type.innerText = tipe.type.name
+    		
+    	elementHtml.appendChild(type)
 
+    	}
+}
+const imgFunction = function(elementHtml, obj,parent){
+    	elementHtml.className ="img-circle , bg-primary";
+    	elementHtml.src = obj.sprites.front_default
+    	parent.appendChild(elementHtml)
+}
+const clearfunction = function(elementHtml){
 
+elementHtml.innerHTML = " "
+}
 /*
 type , nom , image 
-
+separer proprement ! 
+species url -> description ;
 */
